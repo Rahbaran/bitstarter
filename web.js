e96a9ad5949151d62b6bbfb555b22b1e47fd13b2
Rahbar e Rahbaran <farhang_jak@yahoo.de>
2013-07-10 00:30:13 +0200

    minor change

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var buf = new Buffer (fs.readFileSync('/index.html'), 'utf-8');
- 	response.send(buf.toString()); 
+	var buf = new Buffer (fs.readFileSync('index.html'), 'utf-8');
+ 	response.send(buf.toString('utf-8')); 
 });
 
 var port = process.env.PORT || 5000;
